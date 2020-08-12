eeef6aaa43bcfb561a761d5bb0f1b8f6a0d63fda
lucasstd <lucas.std1@gmail.com>
2020-08-11 23:11:57 -0300

    feat: Add styles and props to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/global.css';
+import App from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+import styledNormalize from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 
-export default function MyApp({ Component, pageProps }) { 
-    return <Component {...pageProps} /> 
+const NormalizeStyle = createGlobalStyle`${styledNormalize}`;
+
+class MainApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const cookies = ctx && ctx.req ? ctx.req.headers.cookie : "";
+    const isServer = !process.browser;
+    const componentInitProps = Component.getInitialProps
+      ? await Component.getInitialProps({ ...ctx, isServer })
+      : {};
+    return {
+      cookies,
+      pageProps: componentInitProps,
+    };
+  }
+
+  render() {
+    const { Component, pageProps, cookies, router } = this.props;
+    const defaultTitle = "Advogada Daiana Staudt";
+    return (
+      <>
+        <Head>
+          <title>{defaultTitle}</title>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Advogada situada em porto alegre" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content={defaultTitle} />
+        </Head>
+        <Component router={router} {...pageProps} />
+      </>
+    );
+  }
 }
+
+export default MainApp;
