12e9e5ec11ad98b6f18f353d2e010a865cb46129
lucasstd <lucas.std1@gmail.com>
2020-08-11 23:32:48 -0300

    fix: Common pages unecessary items
    replace bad code to eslint code and replace unused div

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <a href="https://www.linkedin.com/in/daiana-staudt-ab520017/" target="_blank" alt="pagina do linkedin">
-                    Linkedin
-                </a>
-                <a href="https://www.facebook.com/daiana.staudt" target="_blank" alt="pagina do facebook">
-                    Facebook
-                </a>
-            </footer>
-        );
-    }
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <a
+            href="https://www.linkedin.com/in/daiana-staudt-ab520017/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <a href="https://www.facebook.com/daiana.staudt" target="_blank">
+            Facebook
+          </a>
+        </div>
+      </footer>
+    );
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import Footer from '../components/common/Footer';
-import Headers from '../components/common/HtmlHead';
-import Header from '../components/common/Navbar';
-
+import React, { Component } from "react";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Navbar";
 
 export default class Home extends Component {
   render() {
     return (
       <>
-        <Headers />
-
         <Header />
         <img src="/bg.jpg" alt="imagem de fundo" />
         <main>
