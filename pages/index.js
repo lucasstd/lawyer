d4e630adcc834b5961a3e083b38004b9e1886c22
lucasstd <lucas.staudt@rocordoni.local>
2020-08-15 20:10:30 -0300

    fix: replace img src location

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,13 @@ export default class Home extends Component {
     return (
       <>
         <Header />
-        <img src="/bg.jpg" alt="imagem de fundo" />
+        
+        <img className="max-width" src="/images/bg.jpg" alt="imagem de fundo" style={{ width: "100%"}} />
         <main>
           <p>-</p>
         </main>
+
+        <Footer />
       </>
     );
   }
